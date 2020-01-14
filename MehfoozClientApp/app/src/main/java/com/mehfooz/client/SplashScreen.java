e0c96cf76475885190ea7b79d531e81381cb9a68
Ajwad <shaikhajwad10@gmail.com>
2020-01-15 02:44:26 +0530

    Initialized Android Client App

---
+++ MehfoozClientApp/app/src/main/java/com/mehfooz/client/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.mehfooz.client;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
