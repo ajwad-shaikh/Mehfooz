32b7970daeb7a098c22e35f5a17f67af4c6e1e17
Ajwad <shaikhajwad10@gmail.com>
2020-01-15 03:04:58 +0530

    Migrated to AndroidX and Added MapFragment to MainActivity

--- MehfoozClientApp/app/src/main/java/com/mehfooz/client/SplashScreen.java
+++ MehfoozClientApp/app/src/main/java/com/mehfooz/client/SplashScreen.java
@@ -3,11 +3,9 @@ package com.mehfooz.client;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SplashScreen extends AppCompatActivity {
 
